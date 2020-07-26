bd8dc55e1a8dbd96a915a665380abb282def9caf
Bhargavia <bhargavi.angadi15@gmail.com>
2020-07-26 15:38:59 -0700

    add user registration function

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,41 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+	apiKey: "AIzaSyDRIl07gZ089aSdIqautqQvt7QS-39S-Uw",
+	authDomain: "socialmediaapp-dc944.firebaseapp.com",
+	databaseURL: "https://socialmediaapp-dc944.firebaseio.com",
+	projectId: "socialmediaapp-dc944",
+	storageBucket: "socialmediaapp-dc944.appspot.com",
+	messagingSenderId: "365392224269",
+	appId: "1:365392224269:web:0f7f954666520eb1757544",
+	measurementId: "G-YDLTHEC7JG",
+}; 
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/notifications',(req,res) => {
-    admin
-			.firestore()
-            .collection("notifications")
-            .orderBy('createdAt', 'desc')
-			.get()
-			.then((data) => {
-				let notification_list = [];
-				data.forEach((doc) => {
-					notification_list.push({
-                        notificationId: doc.id,
-                        ...doc.data()
-                    });
-				});
-				return res.json(notification_list);
-			})
-			.catch((err) => console.log(err));
+    db
+    .collection("notifications")
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then((data) => {
+        let notification_list = [];
+        data.forEach((doc) => {
+            notification_list.push({
+                notificationId: doc.id,
+                ...doc.data()
+            });
+        });
+        return res.json(notification_list);
+    })
+    .catch((err) => console.log(err));
 })
 
 app.post('/notifications',(req,res) => {
@@ -31,7 +44,7 @@ app.post('/notifications',(req,res) => {
         message: req.body.message,
         createdAt: new Date().toISOString()
     }
-    admin.firestore().collection('notifications').add(newNotification).then(doc => {
+   db.collection('notifications').add(newNotification).then(doc => {
         console.log(`New Notification with ID ${doc.id} is created succesfully`)
         return res.json(
 					`New Notification with ID ${doc.id} is created succesfully`
@@ -40,6 +53,58 @@ app.post('/notifications',(req,res) => {
         res.status(500).json( {error: 'something went wrong' });
         console.log(err);
     })
+})
+
+
+//sign-up route
+app.post('/signup', (req,res) => {
+    const newUser = {
+			email: req.body.email,
+			password: req.body.password,
+			confirmPassword: req.body.confirmPassword,
+			userName: req.body.userName
+        };
+        //TODO: validate data
+
+        let tokenId,userId;
+
+        db.doc(`/users/${newUser.userName}`).get()
+            .then(doc => {
+                if(doc.exists) {
+                    return res.status(400).json({message: `${newUser.userName} already exists`})
+                }else{
+                  return   firebase
+                            .auth()
+                            .createUserWithEmailAndPassword(
+                                newUser.email,
+                                newUser.password
+                            );
+                }
+            })
+            .then(data => {
+                userId = data.user.uid;
+                return data.user.getIdToken()
+            })
+            .then(token => {
+                tokenId = token;
+                const newUserCreditials = {
+                    userName: newUser.userName,
+                    userId: userId,
+                    emailId: newUser.email,
+                    createdAt: new Date().toISOString(),
+                };
+                return db.doc(`/users/${newUser.userName}`).set(newUserCreditials);
+            })
+            .then(() => {
+                return res.status(200).json({token: tokenId})
+            })
+            .catch((err) => {
+                if(err.code === "auth/email-already-in-use") {
+                    return res.status(400).json({ errorMessage: "Email already in use"})
+                }else{
+                    return res.status(500).json({errorMessage: err.code})
+                }       
+            })
 })
 
 //https://baseurl.com/api/<endpoint>
